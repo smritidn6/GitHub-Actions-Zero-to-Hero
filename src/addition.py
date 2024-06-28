0cb32af3fe73f984f85a8ec0b0b137c175fa9315
smritidn6 <155803030+smritidn6@users.noreply.github.com>
2024-06-28 17:52:33 +0530

    Update addition.py

--- src/addition.py
+++ src/addition.py
@@ -1,8 +1,9 @@
 # app.py
-# This is a test commit -new1
+# This is a test commit
 def add(a, b):
     return a + b
 
 def test_add():
     assert add(1, 2) == 3
     assert add(1, -1) == 0
+# adding a new line
